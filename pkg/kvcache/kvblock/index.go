d18deffcf499de106059782e373c54fffb1b5ba0
sallyom <somalley@redhat.com>
2025-07-28 10:35:57 -0400

    Initial distributed tracing instrumentation
    - Add OpenTelemetry distributed tracing support to kv-cache-manager
    - Instrument Redis operations with automatic OTel tracing and metrics

--- pkg/kvcache/kvblock/index.go
+++ pkg/kvcache/kvblock/index.go
@@ -49,6 +49,7 @@ type IndexConfig struct {
 func DefaultIndexConfig() *IndexConfig {
 	return &IndexConfig{
 		InMemoryConfig: DefaultInMemoryIndexConfig(),
+		RedisConfig:    DefaultRedisIndexConfig(),
 		EnableMetrics:  false,
 	}
 }
